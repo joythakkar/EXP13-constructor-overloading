c8651e314847bbe9bd91b79624697fbcd189bfcf
Joy Thakkar <joythakkar20406@gmail.com>
2025-09-04 10:20:42 +0530

    Create Overloading.cpp

---
+++ Overloading.cpp
@@ -0,0 +1,31 @@
+//Joy Thakkar
+//24070123051
+#include <iostream>
+using namespace std;
+
+class Add {
+private:
+    double sum;
+public:
+   
+    Add(int a, int b,int c) {
+        sum = a + b+c;
+        cout << "Sum (double) = " << sum << endl;
+    }
+
+
+    Add(int a, int b) {
+        sum = a + b;
+        cout << "Sum (int) = " << sum << endl;
+    }
+};
+    
+int main() {
+    Add A1(5900000, 60000000,10);  
+    Add A2(5, 10);                 
+    return 0;
+}
+
+//Output
+//Sum (double) = 6.59e+07
+//Sum (int) = 15
