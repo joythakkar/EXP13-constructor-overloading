9adb0f6849cf1cd270eada2a2eb42732f2ee3e4d
Joy Thakkar <joythakkar20406@gmail.com>
2025-09-04 10:18:06 +0530

    Create Function overloading.cpp

---
+++ Function overloading.cpp
@@ -0,0 +1,30 @@
+//Joy Thakkar
+//24070123051
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+
+void calc(int a,int b){
+    int sum=a+b;
+    cout<< sum<<endl;
+}
+
+void calc(int a,int b,int c)
+{
+    int diffrence=a-b-c;
+    cout<< diffrence;
+}
+
+int main()
+{
+    
+     calc(10,20);
+     calc(20,10,5);
+    
+}
+
+//Output
+//30
+//5
